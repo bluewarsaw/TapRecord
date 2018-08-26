26c32008900968214a31b80f17feb7bc4bd41769
Sebastian Czarnecki <32302475+bluewarsaw@users.noreply.github.com>
2018-08-26 13:40:26 +0200

    Initial commit

---
+++ app/src/main/java/pl/sebastianczarnecki/app_rxjava/User.java
@@ -0,0 +1,12 @@
+package pl.sebastianczarnecki.app_rxjava;
+
+import android.location.Address;
+
+public class User {
+    String name;
+    String email;
+    String gender;
+    Address address;
+
+    // getters and setters
+}
